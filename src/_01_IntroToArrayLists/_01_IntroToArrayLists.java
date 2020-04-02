936c8111961325bb0e4038895bd093ad8f3060a9
Corbin <Corbin@DESKTOP-3ATA69G.home>
2020-04-01 20:02:35 -0700

    finished all up to guestbook

--- src/_01_IntroToArrayLists/_01_IntroToArrayLists.java
+++ src/_01_IntroToArrayLists/_01_IntroToArrayLists.java
@@ -6,18 +6,37 @@ public class _01_IntroToArrayLists {
 	public static void main(String[] args) {
 		//1. Create an array list of Strings
 		//   Don't forget to import the ArrayList class
-
+		ArrayList<String> strings = new ArrayList<String>();
 		//2. Add five Strings to your list
-		
+		strings.add("Uno");
+		strings.add("Dos");
+		strings.add("Tres");
+		strings.add("Cuatro");
+		strings.add("Cinco");
 		//3. Print all the Strings using a standard for-loop
-		
+		for (int i = 0; i < strings.size(); i++) {
+			System.out.println(strings.get(i));
+		}
 		
 		//4. Print all the Strings using a for-each loop
-		
+		for (String s : strings) {
+		System.out.println(s);
+		}
 		//5. Print only the even numbered elements in the list.
-		
+		for (int i = 0; i < strings.size(); i++) {
+			if(i%2!=0) {
+				System.out.println(strings.get(i));
+			}
+		}
 		//6. Print all the Strings in reverse order.
-		
+		for (int i = strings.size()-1; i >= 0; i--) {
+			System.out.println(strings.get(i));
+		}
 		//7. Print only the Strings that have the letter 'e' in them.
+		for (String s : strings) {
+			if(s.contains("e")) {
+				System.out.println(s);
+			}
+		}
 	}
 }
--- src/_01_IntroToArrayLists/_02_GuestBook.java
+++ src/_01_IntroToArrayLists/_02_GuestBook.java
@@ -1,5 +1,11 @@
 package _01_IntroToArrayLists;
 
+import java.awt.Panel;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
 public class _02_GuestBook {
 	// Create a GUI with two buttons. One button reads "Add Name" and the other button reads "View Names". 
 	// When the add name button is clicked, display an input dialog that asks the user to enter a name. Add
@@ -9,6 +15,19 @@ public class _02_GuestBook {
 	// Guest #2: Sandy Summers
 	// Guest #3: Greg Ganders
 	// Guest #4: Donny Doners
+	
+	public static void main(String[] args) {
+		JPanel panel = new JPanel();
+		JFrame frame = new JFrame();
+		JButton button1 = new JButton();
+		JButton button2 = new JButton();
+		
+		frame.add(panel);
+		panel.add(button1);
+		panel.add(button2);
+		button1.setText("Add Name");
+		button2.setText("View Names");
+	}
 	
 	
 	
